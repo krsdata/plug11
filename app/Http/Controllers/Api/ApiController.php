2a4f5618601cb2e05de222e7f6bdc31a3f38a15a
krsdata <kroy.iips@gmail.com>
2020-06-05 01:27:00 +0530

    add money

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3145,6 +3145,7 @@ class ApiController extends BaseController
             ->where('match_id',$match_id)
             ->where('user_id',$user_id)
             ->where('contest_id',$contest_id)
+            ->orderBy('rank','desc')
             ->get()
             ->transform(function($item,$key){
                  $prize = \DB::table('prize_distributions')
