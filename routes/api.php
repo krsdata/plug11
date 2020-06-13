8b52d5d3c617f1cffadd6fb75a42e5769dc351e8
krsdata <kroy.iips@gmail.com>
2020-06-14 04:06:04 +0530

    contest cancel

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,7 @@ Route::group([
     'prefix' => 'v2'
 ], function()
 {   
+    Route::match(['post','get'],'getPlaying11', 'Api\ApiController@getPlaying11');
     Route::match(['post','get'],'changeMobile', 'Api\UserController@changeMobile');
     Route::match(['post','get'],'login', 'Api\UserController@login');
         
