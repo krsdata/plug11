0c282b82f16fd96978a0f04f7aea0a0a842bdc47
krsdata <kroy.iips@gmail.com>
2020-06-15 10:59:53 +0000

    api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With, auth-token');
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Origin: *");
+
 
 Route::group(['prefix' => 'v2'], function () {
     Route::post('/login', 'Api\UserController@login');
