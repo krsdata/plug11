d5d28228cefe0a16a4ace56b76c6d32b27920408
krsdata <kroy.iips@gmail.com>
2020-07-15 21:53:31 +0530

    transaction

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2391,7 +2391,6 @@ class ApiController extends BaseController
             ->whereIn('match_id',
                 \DB::table('join_contests')->where('user_id',$user_id)
                     ->where('created_at','>=',$date)
-                    ->orderBy('id','desc')
                     ->groupBy('match_id')
                     ->pluck('match_id')
                     ->toArray()
