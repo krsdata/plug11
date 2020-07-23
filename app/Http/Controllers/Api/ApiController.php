75a61baf6333c3b64c069c25307e1c1e5a7cc012
krsdata <kroy.iips@gmail.com>
2020-07-23 13:19:30 +0530

    prize distribution

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -6056,7 +6056,7 @@ class ApiController extends BaseController
                         ->where('current_status',0)
                         ->where('is_cancelled',0)
                         ->where('timestamp_end','<',time())
-                        ->whereDate('date_start',\Carbon\Carbon::today())
+                        ->whereDate('date_end',\Carbon\Carbon::today())
                         ->get();
               
             if($match->count()){ //dd($match);
