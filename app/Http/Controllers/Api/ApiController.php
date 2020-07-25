243cac4efadc6b4959d8f516db62c3903ff58e6e
krsdata <kroy.iips@gmail.com>
2020-07-26 01:45:56 +0530

    app

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -6086,7 +6086,7 @@ class ApiController extends BaseController
            $user_info = (object)$request->user_info;
             $signature = (object)$request->deviceDetails;  
             $data['eventLog'] = json_encode($request->all());
-            $data['user_id'] = $request->user_id??$user_info->user_id;
+            $data['user_id'] = $user_info->user_id??$request->user_id;
             $data['email'] = $user_info->email??null;
             $data['mobile_number'] = $user_info->mobile_number??null;
             $data['event_name'] = $request->event_name??null;
